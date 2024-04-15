81f518eab9066d7320011fd625271d2edf7cc33f
Marcy <marcy@posteo.de>
2024-04-16 00:13:12 +0200

    Insert UserAgentInfo

---
+++ _user_agent.js
@@ -0,0 +1,12 @@
+/*
+DO NOT COPY THIS COMMENT LINES TO YOUR BOOKMARK
+-----------------------------------------------
+This bookmarklet shows you the user agent you are
+currently using.
+
+@author magicmarcy
+*/
+
+javascript:(() => {
+    alert(window.navigator.userAgent);
+})();
